1184efea570d1158059e8a8359532d3a5c2ac076
Darren Wong <wongdarren1997@gmail.com>
2024-04-07 21:58:49 -0500

    Removed unused unit tests.

--- backend/src/test/java/com/wongdarren/repository/UserRepositoryTest.java
+++
@@ -1,8 +0,0 @@
-package com.wongdarren.repository;
-
-import io.quarkus.test.junit.QuarkusTest;
-
-@QuarkusTest
-public class UserRepositoryTest {
-
-}
--- backend/src/test/java/com/wongdarren/service/UserServiceTest.java
+++
@@ -1,8 +0,0 @@
-package com.wongdarren.service;
-
-import io.quarkus.test.junit.QuarkusTest;
-
-@QuarkusTest
-public class UserServiceTest {
-
-}
