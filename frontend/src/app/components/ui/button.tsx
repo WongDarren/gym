b8573ff861ec9a65e126a3310eb6b2c9761d9945
Darren Wong <wongdarren1997@gmail.com>
2024-04-13 18:29:17 -0500

    Added axios method to get all workouts.

---
+++ frontend/src/app/components/ui/button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+  text: string;
+}
+
+export default function Button({ text }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      {text}
+    </button>
+  );
+}
--- frontend/src/app/workouts/page.tsx
+++ frontend/src/app/workouts/page.tsx
@@ -1,5 +1,77 @@
 'use client';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Button from '@/app/components/ui/button';
+
+interface Set {
+  id: number;
+  dateTime: string;
+  workoutId: number;
+  setNumber: number;
+  weight: number;
+  reps: number;
+  rpe: number;
+  warmup: boolean;
+}
+
+interface Workout {
+  id: number;
+  name: string;
+  dateTime: string;
+  sets: Set[];
+}
+
+interface ApiResponse {
+  workouts: Workout[];
+}
+
+async function getWorkouts(): Promise<Workout[]> {
+  try {
+    const response = await axios.get<ApiResponse>(
+      'http://localhost:8080/api/workouts'
+    );
+    return response.data.workouts;
+  } catch (error) {
+    console.error('Error:', error);
+    return []; // return an empty array in case of an error
+  }
+}
+
 export default function Workouts() {
-  return <div>Workouts</div>;
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+
+  useEffect(() => {
+    void getWorkouts().then(setWorkouts);
+  }, []);
+
+  return (
+    <>
+      <div>Workouts</div>
+      <Button text="Add Workout" />
+      {workouts.map(workout => (
+        <div key={workout.id} className="mb-4">
+          <h2 className="text-2xl font-bold">{workout.name}</h2>
+          <p className="text-gray-500">
+            {new Date(workout.dateTime).toLocaleString()}
+          </p>
+          {workout.sets.map(set => (
+            <div
+              key={set.id}
+              className="mt-2 border-t border-gray-200 bg-gray-100 p-3"
+            >
+              <p className="text-sm">Set Number: {set.setNumber}</p>
+              <p className="text-sm">Weight: {set.weight}</p>
+              <p className="text-sm">Reps: {set.reps}</p>
+              <p className="text-sm">RPE: {set.rpe}</p>
+              <p className="text-sm">Warmup: {set.warmup ? 'Yes' : 'No'}</p>
+              <p className="text-sm">
+                Set Time: {new Date(set.dateTime).toLocaleString()}
+              </p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </>
+  );
 }
