8ac86b6c0c39657182a11bce8bb4fdcc546b519e
Darren Wong <wongdarren1997@gmail.com>
2024-04-13 18:48:34 -0500

    Update set when workout is added.

--- frontend/src/app/workouts/page.tsx
+++ frontend/src/app/workouts/page.tsx
@@ -41,14 +41,16 @@ async function getWorkouts(): Promise<Workout[]> {
 export default function Workouts() {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
 
-  async function createWorkout(name: string): Promise<void> {
+  async function createWorkout(name: string): Promise<Workout> {
     try {
-      await axios.post('http://localhost:8080/api/workouts', { name });
-      // After successfully creating a workout, fetch the updated list of workouts
-      const updatedWorkouts = await getWorkouts();
-      setWorkouts(updatedWorkouts);
+      const response = await axios.post<Workout>(
+        'http://localhost:8080/api/workouts',
+        { name }
+      );
+      return response.data;
     } catch (error) {
       console.error('Error:', error);
+      throw error;
     }
   }
 
@@ -59,28 +61,36 @@ export default function Workouts() {
   return (
     <>
       <div>Workouts</div>
-      <Button text="Add Workout" onClick={() => createWorkout('squat')} />{' '}
+      <Button
+        text="Add Workout"
+        onClick={async () => {
+          const newWorkout = await createWorkout('squat');
+          setWorkouts(prevWorkouts => [...prevWorkouts, newWorkout]);
+        }}
+      />{' '}
       {workouts.map(workout => (
         <div key={workout.id} className="mb-4">
           <h2 className="text-2xl font-bold">{workout.name}</h2>
           <p className="text-gray-500">
             {new Date(workout.dateTime).toLocaleString()}
           </p>
-          {workout.sets.map(set => (
-            <div
-              key={set.id}
-              className="mt-2 border-t border-gray-200 bg-gray-100 p-3"
-            >
-              <p className="text-sm">Set Number: {set.setNumber}</p>
-              <p className="text-sm">Weight: {set.weight}</p>
-              <p className="text-sm">Reps: {set.reps}</p>
-              <p className="text-sm">RPE: {set.rpe}</p>
-              <p className="text-sm">Warmup: {set.warmup ? 'Yes' : 'No'}</p>
-              <p className="text-sm">
-                Set Time: {new Date(set.dateTime).toLocaleString()}
-              </p>
-            </div>
-          ))}
+          {workout.sets.length > 0
+            ? workout.sets.map(set => (
+                <div
+                  key={set.id}
+                  className="mt-2 border-t border-gray-200 bg-gray-100 p-3"
+                >
+                  <p className="text-sm">Set Number: {set.setNumber}</p>
+                  <p className="text-sm">Weight: {set.weight}</p>
+                  <p className="text-sm">Reps: {set.reps}</p>
+                  <p className="text-sm">RPE: {set.rpe}</p>
+                  <p className="text-sm">Warmup: {set.warmup ? 'Yes' : 'No'}</p>
+                  <p className="text-sm">
+                    Set Time: {new Date(set.dateTime).toLocaleString()}
+                  </p>
+                </div>
+              ))
+            : null}
         </div>
       ))}
     </>
