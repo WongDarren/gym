c7387ff009dddeaf4b1d1569db024f2c8abb19f5
Darren Wong <wongdarren1997@gmail.com>
2024-05-05 21:40:50 -0500

    Changed some styles for mobile.

--- frontend/src/app/log/page.tsx
+++ frontend/src/app/log/page.tsx
@@ -19,7 +19,7 @@ export default function LogPage() {
           className="group/btn relative flex h-10 rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          <PlusIcon className="mr-2 h-4 w-4" /> Add workout
+          <PlusIcon className="h-4 w-4" />
           <BottomGradient />
         </Button>
       </div>
--- frontend/src/components/workout-card.tsx
+++ frontend/src/components/workout-card.tsx
@@ -44,12 +44,14 @@ export default function WorkoutCard({
               <span>{new Date(workout.dateTime).toLocaleTimeString()}</span>
             </CardDescription>
           </div>
-          <div>
+          <div className="flex items-center justify-end space-x-3">
+            <TrashIcon className="h-5 w-5 cursor-pointer transition duration-500 hover:text-red-500" />
+            <PencilIcon className="h-5 w-5 cursor-pointer" />
             <Button
-              className="group/btn relative flex h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              className="group/btn relative flex h-10 rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
               type="submit"
             >
-              <PlusIcon className="mr-2 h-4 w-4" /> Add set
+              <PlusIcon className="h-4 w-4" />
               <BottomGradient />
             </Button>
           </div>
